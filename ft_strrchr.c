9e10b7cf16fa537b7a649656eb6ae5f0a86f7a77
gulser <glserozkan@gmail.com>
2022-10-09 20:05:49 +0200

    not sure though

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	int	len;
+	
+	i = 0;
+	len = strlen(s) + 1; 
+	while (i < len)
+	{
+		if (s[len - i] == (char)c)
+		{
+			return ((char *)(s + (len -i)));
+		}
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+        const char s[] = "binine";
+        int c = 'i';
+        printf("%s\n", ft_strrchr(s, c));
+        printf("%s\n", strrchr(s, c));
+}
+
