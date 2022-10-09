16cd5bcedc65b0ea5d07e71bb9c48547a3107702
gulser <glserozkan@gmail.com>
2022-10-09 16:07:57 +0200

    not sure though

---
+++ ft_strchr.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+	int	len_s;
+
+	i = 0;
+	len_s = ft_strlen(s) + 1; // +1 for '/0'
+	
+	while (i < len_s)
+	{
+		if (s[i] == (char)c)
+		{
+			return ((char*)(s + i));
+		}
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+        const char s[] = "banana";
+        int c = 'n';
+        printf("%s\n", ft_strchr(s, c));
+        printf("%s\n", strchr(s, c));
+}
