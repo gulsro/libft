21a475eb30f88a5f78288d4948dc96d18772afe3
Codam pc <glserozkan@gmail.com>
2022-10-14 14:04:44 +0200

    not sure for sure

---
+++ ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strdup.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gozturk <marvin@codam.nl>                    +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/14 13:35:03 by gozturk       #+#    #+#                 */
+/*   Updated: 2022/10/14 14:04:16 by gozturk       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char *dst;
+	size_t	len;
+
+	len = strlen(s1) + 1;
+	dst = (char *)malloc(sizeof(char) * (len));
+	if (dst)
+		memcpy(dst, s1, len);
+		return (dst);
+
+}
+
+int main()
+{
+	const char s1[13] = "lemonade";
+
+	printf("%s\n", ft_strdup(s1));
+	printf("%s\n", strdup(s1));
+}
