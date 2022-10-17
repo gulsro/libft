8021017eae40ac4b28b6c193c7d9dc9a2a71e6bd
Codam pc <glserozkan@gmail.com>
2022-10-17 13:50:44 +0200

    updated

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,32 +6,25 @@
 /*   By: gozturk <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/13 15:19:42 by gozturk       #+#    #+#                 */
-/*   Updated: 2022/10/13 15:59:04 by gozturk       ########   odam.nl         */
+/*   Updated: 2022/10/17 13:49:49 by gozturk       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <strings.h>
 #include <stdio.h>
+#include <string.h>
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-	char	*s_1;
-
-	s_1 = s;
-	i = 0;
-	while (i < n)
-	{
-		s_1[i] = 0;
-		i++;		
-	}
+	memset(s, 0, n);
 }
-
+/*
 int main()
 {
 	size_t  n = 9;
     char *s = "";
 
-	printf("%p\n", ft_bzero(s, n));
+	printf("%s\n", ft_bzero(s, n));
 	printf("%s\n", bzero(s, n));
 }
+*/
