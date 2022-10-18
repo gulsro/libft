24ec82bea74df1d2b9646c27167a4e0679081fcb
gulser <glserozkan@gmail.com>
2022-10-18 19:32:48 +0200

    very bad idea

--- ft_itoa.c
+++ ft_itoa.c
@@ -15,14 +15,41 @@
 
 char	*ft_itoa(int n)
 {
-	char *s;
-	int	i;
-	int	sign;
-
-	sign = 1;
+	//int	sign;
+	int	count;
+	int	len;
+	int 	i;
+	char	*s;
+	
+	//sign = 1;
 	i = 0;
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	len = count + 1;
+	s = malloc((len + 1) * sizeof(int));
+	if (!s)
+		return (0);
 	if (n < 0)
-		sign = -1;
-	if (n > 9)
-
+		sign = -1;)
+	while (s)
+	{
+		s[i] = n % 10 + '0';
+		i++;
+	}	
+	s[i] = '\0';
+	while (i < len / 2)
+	{
+		s[i] = s[len - 1];
+		i++;
+		len--;
+	}	
+	return (s);
+}
+int main()
+{
+	int n = 65;
+	printf("%s\n", ft_itoa(n));
 }
