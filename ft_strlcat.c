ffa041340eb944a8ef14e35c8ec059ef1d92e9a9
Codam pc <glserozkan@gmail.com>
2022-10-19 13:42:05 +0200

    f. STRLCAT

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,5 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strlcat.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gozturk <marvin@codam.nl>                    +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/19 11:19:46 by gozturk       #+#    #+#                 */
+/*   Updated: 2022/10/19 13:37:43 by gozturk       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-//#include "libft.h"
+#include "libft.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -7,7 +18,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
-	size_t	result;
 	size_t	dst_len;
 	size_t	src_len;
 
@@ -15,35 +25,36 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst_len = strlen(dst);
 	i = 0;
 	j = dst_len;
-	
-// CHECK THE MANUAL AGAIN
-	while (dstsize >= src_len + dst_len + 1)
+	if (src_len == 0)
+		return (dst_len + src_len);
+	if (dstsize < dst_len + 1)
+		return (dstsize + src_len); // why not DSTLEN + srclen?
+	if (dstsize > dst_len + 1)
 	{
-		if (dst[j] == 0 && src[i] == 0)
-			return (dst_len + src_len);
-		while (dst[j] && src[i])
+		while (src[i] != '\0' && i < (dstsize - dst_len - 1)) 
 		{
 			dst[j] = src[i];
 			i++;
 			j++;
 		}
 		dst[j] = '\0';
-		result = strlen(dst);
-		return (result);
+		return (dst_len + src_len);
 	}
+	return (0);
 }
+/*
 int main ()
 {
 	char dst[15] = "banana";
 	const char src[] = "lemonade";
-	size_t	dstsize = 5;
+	size_t	dstsize = 4;
 
 	printf("%lu\n", ft_strlcat(dst, src, dstsize));
 	printf("%s\n", dst);
 	char ds[15] = "banana";
 	printf("%lu\n", strlcat(ds, src, dstsize));
 	printf("%s\n", ds);
+	printf("%s\n", src);
 
 }
-
-
+*/
