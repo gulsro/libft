146d9d88efb93c423aff95c666f2f3450d4b0447
Codam pc <glserozkan@gmail.com>
2022-10-24 15:29:21 +0200

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,50 +6,67 @@
 /*   By: gozturk <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/18 17:40:10 by gozturk       #+#    #+#                 */
-/*   Updated: 2022/10/19 15:18:55 by gozturk       ########   odam.nl         */
+/*   Updated: 2022/10/24 15:22:22 by gozturk       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <string.h>
-
-char	*ft_itoa(int n)
+#include <stdio.h>
+static	int	int_len(int n)
 {
-	//int	sign;
 	int	count;
-	int	len;
-	int 	i;
-	char	*s;
-	
-	//sign = 1;
-	i = 0;
+
+	count = 0;
+	if (n <= 0)
+	{
+		count = 1;
+	}
 	while (n != 0)
 	{
 		n /= 10;
 		count++;
 	}
-	len = count + 1;
-	s = malloc((len + 1) * sizeof(int));
-	if (!s)
-		return (0);
-	if (n < 0)
-		sign = -1;)
-	while (s)
-	{
-		s[i] = n % 10 + '0';
-		i++;
-	}	
-	s[i] = '\0';
-	while (i < len / 2)
-	{
-		s[i] = s[len - 1];
-		i++;
-		len--;
-	}	
-	return (s);
+	return (count);
 }
+char	*ft_itoa(int n)
+{
+	int	len;
+	int	numb;
+	char	*s;
+
+	numb = n;	
+	len = int_len(n);
+	if (n < 0)
+		numb = n * -1;
+	s = malloc((len + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	if (n == -2147483648)
+	{
+		memcpy(s, "-2147483648", 12 );		//	s = "-2147483648"
+		return (s);
+	}
+	while (len >= 1)
+	{
+		s[len - 1] = numb % 10 + '0';
+		if (numb >= 10)
+			numb /= 10;
+		len--;
+	}
+	len = int_len(n);
+	s[len] = '\0';
+	if (n < 0)
+	{
+		s[0] = '-';
+	}
+	return (s);
+
+}
+/*
 int main()
 {
-	int n = 65;
+	int n = -2147483648;
 	printf("%s\n", ft_itoa(n));
 }
+*/
