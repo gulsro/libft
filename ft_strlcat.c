5e29af9fe6ebad82d6604e50957460d46294477d
Gulser Ozturk <gozturk@f0r4s22.codam.nl>
2022-10-27 17:50:30 +0200

    worked :D

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gozturk <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/19 11:19:46 by gozturk       #+#    #+#                 */
-/*   Updated: 2022/10/26 17:53:22 by gozturk       ########   odam.nl         */
+/*   Updated: 2022/10/27 17:49:54 by gozturk       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	src_len;
 
-	src_len = strlen(src);
-	dst_len = strlen(dst);
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
 	i = 0;
 	j = dst_len;
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
 	if (dstsize > dst_len)
 	{
 		while (src[i]  && i < (dstsize - dst_len - 1)) 
@@ -36,21 +38,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[j] = '\0';
 		return (dst_len + src_len);
 	}
-	if (dstsize <= dst_len)
-		return (dstsize + src_len);
-	return (src_len);	
+	return (0);	
 }
 /*
 int main ()
 {
-	char dst[15] = "aaaaaaaaaaqqq11";
-	const char src[] = "lorem ipsum dolor sit amet";
-	size_t	dstsize = 5;
+    char dst1[15] = "rrrrrrrrrrrrrrr";
+    char dst2[15] = "mrlemonadecaker";
+    const char src[] = "lorem ipsum dolor sit amet";
+    size_t    dstsize = 5;
 
-	printf("me=%lu\n", ft_strlcat(dst, src, dstsize));
-	printf("me=%s\n", dst);
-	char ds[15] = "aaaaaaaaaaqqq11";
-	printf("fn=%lu\n", strlcat(ds, src, dstsize));
-	printf("fn=%s\n", ds);
+    printf("me=%lu\n", ft_strlcat(dst1, src, dstsize));
+    printf("me=%s\n", dst1);
+    printf("fn=%lu\n", strlcat(dst2, src, dstsize));
+    printf("fn=%s\n", dst2);
 }
 */
