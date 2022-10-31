a2bd83020a21dcd403cd3391f292628c0d5e2f44
gulser <glserozkan@gmail.com>
2022-10-31 07:41:04 +0100

    new

--- ft_split.c
+++ ft_split.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "libft.h"
-
-// :%s/foo/bar/g
 
 int find_str_pos(const char* input_str, int start, char c)
 {
-    if (start < 0 || start >= (int)ft_strlen(input_str))
+    if (start < 0 || start >= (int)strlen(input_str))
     {
         return -1;
     }
@@ -23,57 +20,77 @@ int find_str_pos(const char* input_str, int start, char c)
     }
     return -1;
 }
-void get_substr(const char* input_str, char* dest, int start, int len)
+char* get_substr(const char* input_str, int start, int len)
 {
-    dest = malloc(len+1);
+    char* dest = malloc(len+1);
     strncpy(dest, &input_str[start], len);
-//    dest[len] = '\0';
-//    printf("sub_input_str: %s\n",dest);
+    dest[len] = '\0';
+    printf("sub_input_str: %s\n",dest);
+    return dest;
 }
-char** ft_split(const char* input_str, char token)
+char **resize(char** old_result, int elem_count)
+{
+    char **new_result;
+    int counter=0;
+    
+    new_result = malloc(elem_count);
+    while(counter < elem_count)
+    {
+        new_result[counter] = malloc(strlen(old_result[counter]));
+        strcpy(new_result[counter], old_result[counter]);
+        free(old_result[counter]);
+        counter++;
+    }
+    free(old_result);
+    return new_result;
+}
+char** my_split(const char* input_str, char token)
 {
     int start = 0;
     int pos = 0;
-    int temp_size = ft_strlen(input_str);
+    int temp_size = 0;
     int elem_count = 0;
     char **result;
+    if (input_str == 0)
+    {
+        return 0;
+    }
+    result = malloc(strlen(input_str));
 
-	result = malloc(ft_strlen(input_str));
-	
     while (pos != -1)
     {
         pos = find_str_pos(input_str, pos, token);
 
-        if (pos != -1) 
+        if (pos != -1)
         {
             pos++;
             temp_size = pos -1 - start;
             if (temp_size > 0)
             {
-                get_substr(input_str, result[elem_count], start, temp_size);
+                result[elem_count] = get_substr(input_str, start, temp_size);
+                elem_count++;
             }
             start = pos;
-        } else
+        } else if (elem_count == 0)
         {
-            get_substr(input_str, result[elem_count], start, temp_size);     
+            result[elem_count] = get_substr(input_str, start, strlen(input_str));
+            elem_count++;    
         }
-     elem_count++;
-    }
 
-//    result[elem_count] = "";
-    return result;
+    }
+    printf("elem_count:%d \n",elem_count);
+    return resize(result, elem_count);
 } 
-/*
-int main(int argc, char const *argv[])
+
+int main()
 {
-    const char* input_str = "abxcdxxefxgh";
-    char c = 'x';
+    const char* input_str = " 0 0 0 0 0 0 0 0 0";
+    char c = ' ';
 
     printf("original: %s\n", input_str);
-    char** res = ft_split(input_str, c);
+    char** res = my_split(input_str, c);
 
     free(res);
 
     return 0;
 }
-*/
