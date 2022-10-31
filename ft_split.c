2de15aa6fe419de8ca0be11aafed7ad81b13ab7a
gulser <glserozkan@gmail.com>
2022-10-31 11:14:38 +0100

    new one

--- ft_split.c
+++ ft_split.c
@@ -1,96 +1,34 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "libft.h"
 
-int find_str_pos(const char* input_str, int start, char c)
+static int	how_many_words(char *str, char c)
 {
-    if (start < 0 || start >= (int)strlen(input_str))
-    {
-        return -1;
-    }
-    int pos = start;
-    
-    while(*(input_str + pos) != '\0')
-    {
-        if (*(input_str + pos) == c)
-        {
-            return pos;
-        }
-        pos++;
-    }
-    return -1;
+	int	i;
+	int	count;
+	
+	i = 1; 		// because str[0] already checked
+	count = 0;
+	if (!str)
+		return (0);
+	if (str[0] != c)
+		count++;
+	while (str[i] != '\0')
+	{
+		if (str[i] != c && str[i - 1] == c)
+			count++;
+		i++;
+	}
+	return (count);
 }
-char* get_substr(const char* input_str, int start, int len)
+
+static int	word_lenght(char *str, char c)
 {
-    char* dest = malloc(len+1);
-    strncpy(dest, &input_str[start], len);
-    dest[len] = '\0';
-    printf("sub_input_str: %s\n",dest);
-    return dest;
+	int	i;
+	
+	i = 0;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	return (i);
 }
-char **resize(char** old_result, int elem_count)
-{
-    char **new_result;
-    int counter=0;
-    
-    new_result = malloc(elem_count);
-    while(counter < elem_count)
-    {
-        new_result[counter] = malloc(strlen(old_result[counter]));
-        strcpy(new_result[counter], old_result[counter]);
-        free(old_result[counter]);
-        counter++;
-    }
-    free(old_result);
-    return new_result;
-}
-char** my_split(const char* input_str, char token)
-{
-    int start = 0;
-    int pos = 0;
-    int temp_size = 0;
-    int elem_count = 0;
-    char **result;
-    if (input_str == 0)
-    {
-        return 0;
-    }
-    result = malloc(strlen(input_str));
 
-    while (pos != -1)
-    {
-        pos = find_str_pos(input_str, pos, token);
-
-        if (pos != -1)
-        {
-            pos++;
-            temp_size = pos -1 - start;
-            if (temp_size > 0)
-            {
-                result[elem_count] = get_substr(input_str, start, temp_size);
-                elem_count++;
-            }
-            start = pos;
-        } else if (elem_count == 0)
-        {
-            result[elem_count] = get_substr(input_str, start, strlen(input_str));
-            elem_count++;    
-        }
-
-    }
-    printf("elem_count:%d \n",elem_count);
-    return resize(result, elem_count);
-} 
-
-int main()
-{
-    const char* input_str = " 0 0 0 0 0 0 0 0 0";
-    char c = ' ';
-
-    printf("original: %s\n", input_str);
-    char** res = my_split(input_str, c);
-
-    free(res);
-
-    return 0;
-}
+static
