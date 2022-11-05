404368d51734a3ddcb5eb03f24c69f5137727c96
gulser <glserozkan@gmail.com>
2022-11-05 19:17:59 +0100

    while loop statement order

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return ((char *)(haystack));
 	if (ft_strlen(haystack) < len_n)
 		return (0);
-	while (haystack[i] && i < len)
+	while (i < len && haystack[i])
 	{
 		if (haystack[i] == needle[0])
 		{
